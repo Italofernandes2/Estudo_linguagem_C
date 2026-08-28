3d9e94b37657b7870c182e5d0ad3d430c4cfda47
Italofernandes2 <fitalodi@gmail.com>
2026-08-28 18:14:08 -0300

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int num, suc, ant;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &num);
+    
+    suc = num + 1;
+    ant = num - 1;
+
+    printf("\nO sucessor e: %d", suc);
+    printf("\nO antecessor e: %d", ant);
+
+    return(0);
+}
