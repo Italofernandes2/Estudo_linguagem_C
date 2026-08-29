c178c03c0dea650617531eb404c0442676d0e711
Italofernandes2 <fitalodi@gmail.com>
2026-08-28 23:23:33 -0300

    Add files via upload

---
+++ ex03.c
@@ -0,0 +1,18 @@
+//Programa que calcula a area do circulo
+
+#include <stdio.h>
+
+int main() {
+
+    float raio, area;
+
+    printf("Digite o valor do raio: ");
+    scanf("%f", &raio);
+
+    area = 3.14 * ( raio * raio);
+
+    printf("A area do circulo e: %.2f", area110);
+
+    return (0);
+
+}
